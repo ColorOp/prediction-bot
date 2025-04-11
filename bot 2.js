09a83c658e4583382b623c3b9b388813f8954ad4
ColorOp <pagalworldforu@gmail.com>
2025-04-12 00:13:37 +0530

    Add files via upload

---
+++ bot 2.js
@@ -0,0 +1,24 @@
+const TelegramBot = require('node-telegram-bot-api');
+const token = 'YOUR_BOT_TOKEN'; // Replace with your token
+const bot = new TelegramBot(token, { polling: true });
+
+const colors = ['Red', 'Green', 'Violet'];
+function getPrediction() {
+  return colors[Math.floor(Math.random() * colors.length)];
+}
+
+let users = new Set();
+
+bot.onText(/\/start/, (msg) => {
+  const chatId = msg.chat.id;
+  users.add(chatId);
+  bot.sendMessage(chatId, "Welcome to Prediction Master! You'll now receive color predictions every 5 seconds.");
+});
+
+setInterval(() => {
+  const prediction = getPrediction();
+  const message = `Prediction Master:\nNext Color: *${prediction}*`;
+  users.forEach(chatId => {
+    bot.sendMessage(chatId, message, { parse_mode: 'Markdown' });
+  });
+}, 5000);
